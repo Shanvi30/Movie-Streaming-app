3562ccdc9915b4df507bddc3b565ea1ea3288952
Hanshika Shanvi <hshanvi30@gmail.com>
2026-02-28 16:33:20 +0530

    Firebase Console Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,79 @@
-import React, { useEffect, useState } from 'react'
-import Home from './pages/Home/Home'
-import { Routes, Route, useNavigate, useLocation } from 'react-router-dom'
-import Login from './pages/Login/Login'
-import Player from './pages/Player/Player'
-import MovieDetail from './pages/MovieDetail/MovieDetail'
-import Watchlist from './pages/Watchlist/Watchlist'
-import Movies from './pages/Movies/Movies'
-import TVShows from './pages/TVShows/TVShows'
-import NewPopular from './pages/NewPopular/NewPopular'
-import Settings from './pages/Settings/Settings'
-import NotFound from './pages/NotFound/NotFound'
-import { onAuthStateChanged } from 'firebase/auth'
-import { auth } from './firebase'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import React, { useEffect, useState } from "react";
+import Home from "./pages/Home/Home";
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Player from "./pages/Player/Player";
+import MovieDetail from "./pages/MovieDetail/MovieDetail";
+import Watchlist from "./pages/Watchlist/Watchlist";
+import Movies from "./pages/Movies/Movies";
+import TVShows from "./pages/TVShows/TVShows";
+import NewPopular from "./pages/NewPopular/NewPopular";
+import Settings from "./pages/Settings/Settings";
+import NotFound from "./pages/NotFound/NotFound";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const ScrollToTop = () => {
-  const { pathname } = useLocation()
+  const { pathname } = useLocation();
   useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [pathname])
-  return null
-}
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+};
 
 const App = () => {
-  const navigate = useNavigate()
-  const [showScrollTop, setShowScrollTop] = useState(false)
+  const navigate = useNavigate();
+  const [showScrollTop, setShowScrollTop] = useState(false);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
-        if (window.location.pathname === '/login') navigate('/')
+        if (window.location.pathname === "/login") navigate("/");
       } else {
-        if (window.location.pathname !== '/login') navigate('/login')
+        if (window.location.pathname !== "/login") navigate("/login");
       }
-    })
-    return () => unsubscribe()
-  }, [navigate])
+    });
+    return () => unsubscribe();
+  }, [navigate]);
 
   useEffect(() => {
     const handleScroll = () => {
-      setShowScrollTop(window.scrollY > 400)
-    }
-    window.addEventListener('scroll', handleScroll)
-    return () => window.removeEventListener('scroll', handleScroll)
-  }, [])
+      setShowScrollTop(window.scrollY > 400);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   const scrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' })
-  }
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
 
   return (
     <div>
       <ScrollToTop />
       <ToastContainer theme="dark" />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/player/:id' element={<Player />} />
-        <Route path='/movie/:id' element={<MovieDetail />} />
-        <Route path='/tv/:id' element={<MovieDetail />} />
-        <Route path='/watchlist' element={<Watchlist />} />
-        <Route path='/movies' element={<Movies />} />
-        <Route path='/tvshows' element={<TVShows />} />
-        <Route path='/new-popular' element={<NewPopular />} />
-        <Route path='/settings' element={<Settings />} />
-        <Route path='*' element={<NotFound />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/player/:id" element={<Player />} />
+        <Route path="/movie/:id" element={<MovieDetail />} />
+        <Route path="/tv/:id" element={<MovieDetail />} />
+        <Route path="/watchlist" element={<Watchlist />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/tvshows" element={<TVShows />} />
+        <Route path="/new-popular" element={<NewPopular />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
       {showScrollTop && (
-        <button className='scroll-top-btn' onClick={scrollToTop}>↑</button>
+        <button className="scroll-top-btn" onClick={scrollToTop}>
+          ↑
+        </button>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
