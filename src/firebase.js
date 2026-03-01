6484d79ba894c81be4e8acaaa1d6bc3aaa2ca47a
Hanshika Shanvi <hshanvi30@gmail.com>
2026-03-01 19:35:39 +0530

    fix: authDomain updated

--- src/firebase.js
+++ src/firebase.js
@@ -14,7 +14,7 @@ import { toast } from "react-toastify";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  authDomain: "trailer-streaming.netlify.app",
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
